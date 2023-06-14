f0792fb4f6315d365b368570fb450e496aa757f9
tech628 <136515188+tech628@users.noreply.github.com>
2023-06-14 18:10:42 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,15 +3,62 @@ var golden_scrap1 = 0;
 var level = 0;
 var maxlevel = 100;
 var i = 0;
+var first_up = 0;
 let golden_scrap = document.getElementById("golden-scrap");
 let prestige_container = document.getElementById("prestige-container");
 let collection_prestige_upgrades = document.getElementById("collection-prestige-upgrades")
 let level_bar = document.getElementById("Level-bar")
 let prestige_stars = document.getElementById("prestige-stars")
+let upgrades_barrel = document.getElementById("upgrades-barrel")
 
 function scrapgain() {
     scrap += 1;
     document.getElementById("scrap_gain").textContent = scrap
+    if (golden_scrap1 >= 1) {
+        scrap += 1
+    }
+    if (golden_scrap1 >= 2) {
+        scrap += 1
+    }
+    if (golden_scrap1 >= 3) {
+        scrap += 1
+    }
+    if (golden_scrap1 >= 4) {
+        scrap += 1
+    } 
+    if (golden_scrap1 >= 5) {
+        scrap += 1
+    } 
+    if (golden_scrap1 >= 6) {
+        scrap += 1
+    } 
+    if (golden_scrap1 >= 7) {
+        scrap += 1
+    } 
+    if (golden_scrap1 >= 8) {
+        scrap += 1
+    } 
+    if (golden_scrap1 >= 9) {
+        scrap += 1
+    } 
+    if (golden_scrap1 >= 10) {
+        scrap += 1
+    } 
+    if (first_up >= 1) {
+        scrap += 9
+    }
+    if (first_up >= 2) {
+        scrap += 10
+    }
+    if (first_up >= 3) {
+        scrap += 10
+    }
+    if (first_up >= 4) {
+        scrap += 10
+    }
+    if (first_up >= 5) {
+        scrap += 10
+    }
 }
 
 if (scrap >= 100) {
@@ -22,10 +69,8 @@ function golden_scrapGain() {
     if( scrap >= 100) {
         golden_scrap1 += 1
         scrap -= scrap
+        first_up -= first_up
         document.getElementById("paragraph-golden-scrap-text").textContent = golden_scrap1
-    }
-    if ( golden_scrap1 >= 1 ) {
-        scrap += 2
     }
 }
 
@@ -55,6 +100,14 @@ function golden_scrapclicked() {
     golden_scrap.classList.remove("open-golden-scrap")
 }
 
+function close_barrelUpgrades() {
+    upgrades_barrel.classList.remove("open-upgrades-barrel")
+}
+
+function open_barrelUpgrades() {
+    upgrades_barrel.classList.add("open-upgrades-barrel")
+}
+
 function setItem() {
     localStorage.setItem("scrap_gain", scrap, "collection-prestige-upgrades",collection_prestige_upgrades, "golden-scrap",golden_scrap1, "paragraph-golden-scrap-text", "Level-bar", level_bar)
 }
@@ -64,11 +117,18 @@ function getItem() {
 }
 
 function stars_toggle() {
-    golden_scrap.classList.add("open-prestige-stars");
-    golden_scrap.classList.remove("golden-scrap-text", "prestige-button")
+    prestige_stars.add("open-prestige-stars");
+    prestige_stars.classList.remove("open-golden-scrap")
 }
 
 function golden_scrap_toggle() {
     golden_scrap.classList.add("open-golden-scrap");
     golden_scrap.classList.remove("open-prestige-stars")
+}
+
+function first_upgrade() {
+    if (scrap >= 100) {
+        scrap -= scrap
+        first_up += 1
+    }
 }
